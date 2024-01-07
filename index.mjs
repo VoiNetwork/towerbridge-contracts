d8685d525b365bfd7b38634166f649af88ef041f
David Runion <david.runion@imedemr.com>
2024-01-07 12:01:43 -0500

    bunch of changes

--- index.mjs
+++ index.mjs
@@ -1,18 +1,12 @@
 import {loadStdlib} from '@reach-sh/stdlib';
 import * as lockBackend from './build/lock.main.mjs';
 import * as mintBackend from './build/mint.main.mjs';
-import { ask, yesno, done } from '@reach-sh/stdlib/ask.mjs';
 import readline from 'readline';
-const stdlib = loadStdlib('ALGO');
+import { ask } from '@reach-sh/stdlib/ask.mjs';
+import dotenv from 'dotenv';
 
-/*stdlib.setSigningMonitor(async (evt, pre, post) => {
-  console.log(evt);
-  console.log(await pre);
-  console.log(await post);
-});*/
-
-const fmt = (x,d) => stdlib.formatCurrency(x, d);
-const getBalance = async (acct) => fmt(await stdlib.balanceOf(acct),6);
+let stdlib = loadStdlib('ALGO');
+dotenv.config();
 
 // Setup readline interface for user input
 const rl = readline.createInterface({
@@ -21,10 +15,34 @@ const rl = readline.createInterface({
 });
 
 // deploy mint contract on Voi
-const deployMintContract = async (acc) => {
-  return new Promise(async (resolve, reject) => {
-    const ctcMint = await acc.contract(mintBackend);
+const deployMintContract = async (network) => {
+  console.log(`Deploying Mint Contract...`);
 
+  let stdlib = loadStdlib('ALGO');
+
+  if (network == 'testnet') {
+    stdlib.setProviderByEnv({
+      ALGO_SERVER: 'https://testnet-api.voi.nodly.io',
+      ALGO_PORT: '',
+      ALGO_TOKEN_HEADER: 'X-Algo-API-Token',
+      ALGO_TOKEN: '',
+      ALGO_INDEXER_SERVER: 'https://testnet-idx.voi.nodly.io',
+      ALGO_INDEXER_PORT: '',
+      ALGO_INDEXER_TOKEN_HEADER: 'X-Indexer-API-Token',
+      ALGO_INDEXER_TOKEN: '',
+      REACH_ISOLATED_NETWORK: 'no',
+      ALGO_NODE_WRITE_ONLY: 'yes',
+    });
+  }
+
+  const acc = await stdlib.newAccountFromMnemonic(process.env.TESTNET_DEPLOYER_MNEMONIC);
+  if (stdlib.balanceOf(acc) < stdlib.parseCurrency(100) && stdlib.canFundFromFaucet(acc)) {
+    await stdlib.fundFromFaucet(acc, stdlib.parseCurrency(100));
+  }
+
+  const ctcMint = await acc.contract(mintBackend);
+
+  await new Promise((resolve, reject) => {
     ctcMint.p.Deployer({
       parameters: {
         sourceTokenId: 10458941,
@@ -42,39 +60,63 @@ const deployMintContract = async (acc) => {
         const ctcMintInfo = await ctcMint.getInfo();
         console.log(`The Mint contract is deployed as = ${JSON.stringify(ctcMintInfo)}`);
 
-        resolve(ctcMint);
+        const [ targetTokenIdMaybe, targetTokenId ] = await ctcMint.v.wrappedToken();
+        stdlib.assert(targetTokenIdMaybe == 'Some', 'no target token minted');
+        console.log(`The target token ID is = ${targetTokenId}`);
+      
+        resolve();
       }
     });
   });
+
+  return ctcMint;
 }
 
 // deploy lock contract on Algorand
-const deployLockContract = async (acc, network, targetTokenId) => {
-  return new Promise(async (resolve, reject) => {
-    let tok;
+const deployLockContract = async (network) => {
+  let stdlib = loadStdlib('ALGO');
 
-    if (network == 'testnet') {
-      tok = await acc.tokenMetadata(10458941);
-    }
-    else {
-      // for devnet we need to mint the token to use for the lock contract
-      tok = await stdlib.launchToken(acc, "DevNet USDC", "USDC", {
-        decimals: 6,
-        supply: stdlib.bigNumberify("18446744073709551615"),
-        url: "https://centre.io",
-        metadataHash: "",
-        clawback: acc.networkAccount,
-        freeze: acc.networkAccount,
-        reserve: acc.networkAccount,
-        manager: acc.networkAccount,
-      });
-    }
+  const targetTokenId = await ask("Enter the target token ID: ");
+  if (targetTokenId == '') {
+    console.log("Invalid target token ID");
+    return;
+  }
 
-    const ctcLock = await acc.contract(lockBackend);
+  if (network == 'testnet') {
+    stdlib.setProviderByName("TestNet");
+  }
 
+  const acc = await stdlib.newAccountFromMnemonic(process.env.TESTNET_DEPLOYER_MNEMONIC);
+  if (stdlib.balanceOf(acc) < stdlib.parseCurrency(100) && stdlib.canFundFromFaucet(acc)) {
+    await stdlib.fundFromFaucet(acc, stdlib.parseCurrency(100));
+  }
+
+  let tok, tokenId;
+  if (network == 'testnet') {
+    //tok = await acc.tokenMetadata(10458941);
+    tokenId = 10458941
+  }
+  else {
+    // for devnet we need to mint the token to use for the lock contract
+    tok = await stdlib.launchToken(acc, "DevNet USDC", "USDC", {
+      decimals: 6,
+      supply: stdlib.bigNumberify("18446744073709551615"),
+      url: "https://centre.io",
+      metadataHash: "",
+      clawback: acc.networkAccount,
+      freeze: acc.networkAccount,
+      reserve: acc.networkAccount,
+      manager: acc.networkAccount,
+    });
+    tokenId = tok.id;
+  }
+
+  const ctcLock = await acc.contract(lockBackend);
+
+  await new Promise(async (resolve, reject) => {
     ctcLock.p.Deployer({
       parameters: {
-        sourceToken: tok.id,
+        sourceToken: tokenId,
         sourceChainId: 1, // 1 = Algorand
         targetChainId: 2, // 2 = Voi
         targetTokenId: targetTokenId, // Token ID on Voi
@@ -91,72 +133,92 @@ const deployLockContract = async (acc, network, targetTokenId) => {
   });
 }
 
-const deployContracts = async (network) => {
-  // === DEPLOY MINT CONTRACT ===
-  if (network == 'testnet') {
-    stdlib.setProviderByEnv({
-      ALGO_SERVER: 'https://testnet-api.voi.nodly.io',
-      ALGO_PORT: '',
-      ALGO_TOKEN_HEADER: 'X-Algo-API-Token',
-      ALGO_TOKEN: '',
-      ALGO_INDEXER_SERVER: 'https://testnet-idx.voi.nodly.io',
-      ALGO_INDEXER_PORT: '',
-      ALGO_INDEXER_TOKEN_HEADER: 'X-Indexer-API-Token',
-      ALGO_INDEXER_TOKEN: '',
-      REACH_ISOLATED_NETWORK: 'no',
-      ALGO_NODE_WRITE_ONLY: 'yes',
-    });
-  }
-
-  const accMint = await stdlib.newTestAccount(stdlib.parseCurrency(100));
-  const ctcMint = await deployMintContract(accMint);
-  const [ targetTokenIdMaybe, targetTokenId ] = await ctcMint.v.wrappedToken();
-  stdlib.assert(targetTokenIdMaybe == 'Some', 'no target token minted');
-
-  // === DEPLOY LOCK CONTRACT ===
-  if (network == 'testnet') {
-    stdlib.setProviderByName("TestNet");
-  }
-
-  const accLock = await stdlib.newTestAccount(stdlib.parseCurrency(100));
-  await deployLockContract(accLock, network, targetTokenId);
-
-  process.exit();
-}
-
 const testLockContract = async () => {
   console.log('testLockContract');
 
+  let stdlib = loadStdlib('ALGO');
+
+  // connect using TESTNET_USER
+  stdlib.setProviderByName("TestNet");
+  const acc = await stdlib.newAccountFromMnemonic(process.env.TESTNET_USER_MNEMONIC);
+  if (stdlib.balanceOf(acc) < stdlib.parseCurrency(100) && stdlib.canFundFromFaucet(acc)) {
+    await stdlib.fundFromFaucet(acc, stdlib.parseCurrency(100));
+  }
+
+  // get the lock contract
+  const ctcLock = acc.contract(lockBackend, process.env.TESTNET_LOCK_CONTRACT);
+  console.log(ctcLock);
+  
+  // show details
+  const [ sourceTokenIdMaybe, sourceTokenId ] = await ctcLock.v.sourceToken();
+  const [ targetChainIdMaybe, targetChainId ] = await ctcLock.v.targetChainId();
+  const [ targetTokenIdMaybe, targetTokenId ] = await ctcLock.v.targetTokenId(); 
+  const [ bridgedTokensMaybe, bridgedTokens ] = await ctcLock.v.bridgedTokens();
+  console.log(`Lock contract details:`);
+  console.log(`  sourceTokenId = ${sourceTokenId}`);
+  console.log(`  targetChainId = ${targetChainId}`);
+  console.log(`  targetTokenId = ${targetTokenId}`);
+  console.log(`  bridgedTokens = ${bridgedTokens}`);
+
+}
+
+const changeNetwork = async (network) => {
+  await new Promise((resolve, reject) => {
+    rl.question(`Select network (1) TestNet (2) DevNet: `, async (answer) => {
+      switch(answer.toUpperCase()) {
+        case '1':
+          network = 'testnet';
+          console.log('Network changed to TestNet');
+          break;
+        case '2':
+          network = 'devnet';
+          console.log('Network changed to DevNet');
+          break;
+        default:
+          console.log('Invalid choice. Network unchanged.');
+          break;
+      }
+      resolve();
+    });
+  });
+  return network;
 }
 
 const main = async () => {
+  let network = process.env.NETWORK || 'devnet';
+
   console.log(`Tower Bridge`);
+  console.log(`Using network: ${network.toUpperCase()}`);
   console.log(``);
-  console.log(`1. Deploy contracts to DevNet`);
-  console.log(`2. Deploy contracts to TestNet`);
+  console.log(`1. Deploy Mint contract`);
+  console.log(`2. Deploy Lock contract`);
   console.log(`3. Test lock contract`);
+  console.log(`4. Change network`);
   console.log(`Q. Quit`);
 
   const askQuestion = () => {
     rl.question('What would you like to do? ', async (answer) => {
       switch(answer.toUpperCase()) {
         case '1':
-          await deployContracts('devnet');
+          await deployMintContract(network);
           break;
         case '2':
-          await deployContracts('testnet');
+          await deployLockContract(network);
           break;
         case '3':
-          await testLockContract();
+          await testLockContract(network);
+          break;
+        case '4':
+          network = await changeNetwork();
           break;
         case 'Q':
           process.exit();
           break;
         default:
           console.log('Invalid choice');
-          askQuestion();
           break;
       }
+      askQuestion();
     });
   };
 
